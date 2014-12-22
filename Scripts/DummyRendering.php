bcbd891e652c13ba86f5a39d3ee47ca70ebeadda
Helmut Hummel <helmut.hummel@typo3.org>
2014-12-22 18:31:33 +0100

    Revert "[BUGFIX] Initialize page generator in dummy page generation script"
    This reverts commit da4c2750ff8e022c9581a4e82b8ef09136937429.
    
    While the intention is good, the dummy script is the wrong place
    to initialize the rendering, as rendering happens before this
    script is included

--- Scripts/DummyRendering.php
+++ Scripts/DummyRendering.php
@@ -1,9 +1,1 @@
 <?php
-\TYPO3\CMS\Frontend\Page\PageGenerator::pagegenInit();
-// Global content object
-$TSFE->newCObj();
-// LIBRARY INCLUSION, TypoScript
-$temp_incFiles = \TYPO3\CMS\Frontend\Page\PageGenerator::getIncFiles();
-foreach ($temp_incFiles as $temp_file) {
-	include_once './' . $temp_file;
-}
