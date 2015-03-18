23d0b2a6c2c8aa7b650de3c69ebfdbe911a1d1f8
Helmut Hummel <typo3@helmuthummel.de>
2015-03-18 11:19:06 +0100

    Update PHP version dependency
    Fixes #6

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.7-5.5.999',
+			'php' => '5.3.7-5.6.999',
 			'typo3' => '6.0.0-7.99.99',
 		),
 		'conflicts' => array(
