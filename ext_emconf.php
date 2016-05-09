cf85b1254149cbfc8ef0d0ca5284959705776b9a
Helmut Hummel <info@helhum.io>
2016-05-09 22:54:35 +0200

    Convert tabs to spaces

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,30 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'TypoScript Rendering',
-	'description' => 'Can render a TypoScript snippet by URL, especially useful for Ajax dispatching',
-	'category' => 'Rendering',
-	'author' => 'Helmut Hummel',
-	'author_email' => 'info@helmut-hummel.de',
-	'author_company' => 'Helmut Hummel',
-	'shy' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '1.0.5',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.3.7-7.0.999',
-			'typo3' => '6.0.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'TypoScript Rendering',
+    'description' => 'Can render a TypoScript snippet by URL, especially useful for Ajax dispatching',
+    'category' => 'Rendering',
+    'author' => 'Helmut Hummel',
+    'author_email' => 'info@helmut-hummel.de',
+    'author_company' => 'Helmut Hummel',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.5',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.3.7-7.0.999',
+            'typo3' => '6.0.0-8.99.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
-
-?>
