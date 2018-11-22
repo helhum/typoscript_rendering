b6c280f0ddd20f6feb127cade6f5acbe7417ff96
Helmut Hummel <info@helhum.io>
2018-11-22 13:01:31 +0100

    Release 2.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => '0',
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '3.0.0',
+  'version' => '2.2.0',
   'constraints' =>
   array (
     'depends' =>
